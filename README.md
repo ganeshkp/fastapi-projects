26c8c35427cda222abfe0c98c985c64a357491a6
Ganesh Patil <ganeshkumar.patil@gmail.com>
2025-09-06 13:33:56 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # fastapi-projects
 FastAPI learning
+
+# To run file
+fastapi dev main.py
+
+# Check it
+Open your browser at http://127.0.0.1:8000
+
+# Interactive API docs¶
+Now go to http://127.0.0.1:8000/docs.
+
+# Check the openapi.json¶
+If you are curious about how the raw OpenAPI schema looks like, FastAPI automatically generates a JSON (schema) with the descriptions of all your API.
+
+You can see it directly at: http://127.0.0.1:8000/openapi.json.
+
+# Standards-based benefits, alternative documentation¶
+And because the generated schema is from the OpenAPI standard, there are many compatible tools.
+
+Because of this, FastAPI itself provides an alternative API documentation (using ReDoc), which you can access at http://127.0.0.1:8000/redoc
+
---
+++ query_params.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+# Optional parameters
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+
+# Query parameter type conversion
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+
+
+# Multiple path and query parameters
+# @app.get("/users/{user_id}/items/{item_id}")
+# async def read_user_item(
+#     user_id: int, item_id: str, q: str | None = None, short: bool = False
+# ):
+#     item = {"item_id": item_id, "owner_id": user_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+
+
+# Required query parameters
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+
+"""
+In this case, there are 3 query parameters:
+
+needy, a required str.
+skip, an int with a default value of 0.
+limit, an optional int.
+"""
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
