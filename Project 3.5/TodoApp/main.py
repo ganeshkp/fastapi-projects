bb0a79806da0c64905b661a73751197f7564e9d3
Ganesh Patil <ganeshkumar.patil@gmail.com>
2025-10-27 21:18:04 +0000

    upgrade

--- Project 3.5/TodoApp/main.py
+++ Project 3.5/TodoApp/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 import models
 from database import engine
 from routers import auth, todos, admin, owner
@@ -11,3 +12,7 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
 app.include_router(owner.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
